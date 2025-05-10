e2dfda305ca454dbbaf911b228ef553dad2ec5ec
Brenden-Trieu <brenden.trieu@gmail.com>
2025-05-09 21:39:00 -0700

    added chakra

--- glen_group/src/App.js
+++ glen_group/src/App.js
@@ -2,23 +2,28 @@ import { useRef } from "react";
 import Navbar from "./components/Navbar.jsx";
 import Snapshot from "./components/Snapshot.jsx";
 import "./styles/App.css";
+import { ChakraProvider } from '@chakra-ui/react'
+// https://glengroup.ca/
+// https://glengroup.ca/about/
 
 function App() {
   const heroRef = useRef(null);
 
   return (
-    <div>
-      <section ref={heroRef} className="hero">
-        <video id="glenGroupVideo" autoPlay muted loop >
-          <source src="https://glengroup.ca/wp-content/uploads/2023/12/Glen-Group-Final-Video.mp4" type="video/mp4" />
-          <source src="movie.ogg" type="video/ogg" />
-        </video>
-      </section>
-      <Navbar heroRef={heroRef} />
-      <section className="content">
-        <Snapshot />
-      </section>
-    </div>
+    <ChakraProvider>
+      <div>
+        <section ref={heroRef} className="hero">
+          <video id="glenGroupVideo" autoPlay muted loop >
+            <source src="https://glengroup.ca/wp-content/uploads/2023/12/Glen-Group-Final-Video.mp4" type="video/mp4" />
+            <source src="movie.ogg" type="video/ogg" />
+          </video>
+        </section>
+        <Navbar heroRef={heroRef} />
+        <section className="content">
+          <Snapshot />
+        </section>
+      </div>
+    </ChakraProvider>
   );
 }
 
--- glen_group/src/components/Snapshot.jsx
+++ glen_group/src/components/Snapshot.jsx
@@ -1,11 +1,16 @@
 import { useEffect, useState, useRef } from "react";
-// import "../styles/Navbar.css";
+import "../styles/Snapshot.css";
 
 const Navbar = ({ heroRef }) => {
   return (
     <div>
-      <h1>Glen Group Of Companies</h1>
-      <h3>Glen Group Of Companies - A Multi-Faceted Company</h3>
+      <h1 id="sectionTitle">Glen Group Of Companies</h1>
+      <section>
+        <h3>Glen Group Of Companies - A Multi-Faceted Company</h3>
+        <p>
+          Glen Group is a multi-faceted company that provides extraordinary services to cater to all your needs. Our team of experts works diligently to ensure success and customer satisfaction.
+        </p>
+      </section>
     </div>
     
   );
